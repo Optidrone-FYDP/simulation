a32fe015cf202944a2977a6eed059c685b6642f3
CAITLIN WONG <c277wong@uwaterloo.ca>
2025-02-20 00:15:13 -0500

    added timer

--- 3d-sim/flight_sim.py
+++ 3d-sim/flight_sim.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import time
 import numpy as np
 import pyqtgraph.opengl as gl
 import pandas as pd
@@ -10,15 +11,17 @@ from PyQt5.QtWidgets import *
 from OpenGL.GL import *
 from stl import mesh
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.curr_dir = os.path.dirname(os.path.abspath(__file__))
         self.drone_mesh_filename = ""
         self.flight_plan_filename = ""
-        self.init_ui()
         self.flight_plan = []
+        self.time_delay = 0.10
+        self.time_delay_display = 0
+
+        self.init_ui()
 
 
     def init_ui(self):
@@ -32,8 +35,10 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
+        # ROW 0
+
         self.viewer = gl.GLViewWidget()
-        layout.addWidget(self.viewer, 0, 0, 1, 2)
+        layout.addWidget(self.viewer, 0, 0, 1, 3)
 
         self.glGrid = gl.GLGridItem()
         self.glGrid.setSize(200, 200)
@@ -44,35 +49,58 @@ class MainWindow(QMainWindow):
         self.glAxes.setSize(100, 100, 100)
         self.viewer.addItem(self.glAxes)
 
+        # ROW 1
+
         btn_import_drone = QPushButton('Import Drone Mesh', self)
         btn_import_drone.clicked.connect(self.import_drone_mesh)
-        layout.addWidget(btn_import_drone, 1, 0)
+        layout.addWidget(btn_import_drone, 1, 0, 1, 1)
 
         self.lbl_import_drone = QLineEdit(self)
         self.lbl_import_drone.setText("STL file path...")
         self.lbl_import_drone.setReadOnly(True)
-        layout.addWidget(self.lbl_import_drone, 1, 1)
+        layout.addWidget(self.lbl_import_drone, 1, 1, 1, 2)
+
+        # ROW 2
 
         btn_import_flight = QPushButton('Import Flight Plan', self)
         btn_import_flight.clicked.connect(self.import_flight)
-        layout.addWidget(btn_import_flight, 2, 0)
+        layout.addWidget(btn_import_flight, 2, 0, 1, 1)
 
         self.lbl_import_flight = QLineEdit(self)
         self.lbl_import_flight.setText("CSV file path...")
         self.lbl_import_flight.setReadOnly(True)
-        layout.addWidget(self.lbl_import_flight, 2, 1)
+        layout.addWidget(self.lbl_import_flight, 2, 1, 1, 2)
+
+        # ROW 3
 
         btn_start_stop = QPushButton('Start Flight', self)
         btn_start_stop.clicked.connect(self.start_flight)
-        layout.addWidget(btn_start_stop, 3, 0, 1, 2)
+        layout.addWidget(btn_start_stop, 3, 0, 1, 3)
+
+        # ROW 4
+
+        btn_slow_down = QPushButton('Decrease speed by 0.10', self)
+        btn_slow_down.clicked.connect(self.slow_down)
+        layout.addWidget(btn_slow_down, 4, 0)
+
+        self.lbl_speed = QLineEdit(self)
+        self.lbl_speed.setText("0.00 (Default Speed)")
+        self.lbl_speed.setReadOnly(True)
+        layout.addWidget(self.lbl_speed, 4, 1)
+
+        btn_speed_up = QPushButton('Increase speed by 0.10', self)
+        btn_speed_up.clicked.connect(self.speed_up)
+        layout.addWidget(btn_speed_up, 4, 2)
+
+        # ROW 5
 
         btn_reset_all = QPushButton('Reset All', self)
         btn_reset_all.clicked.connect(self.reset_all)
-        layout.addWidget(btn_reset_all, 4, 0)
+        layout.addWidget(btn_reset_all, 5, 0, 1, 1)
 
         btn_reset_flight = QPushButton('Reset Flight', self)
         btn_reset_flight.clicked.connect(self.reset_flight)
-        layout.addWidget(btn_reset_flight, 4, 1)
+        layout.addWidget(btn_reset_flight, 5, 1, 1, 2)
 
         self.show()
 
@@ -152,8 +180,35 @@ class MainWindow(QMainWindow):
 
         for frame in self.flight_plan:
             print(frame)
+            time.sleep(self.time_delay)
             self.mesh.translate(frame[0], frame[1], frame[2])
             app.processEvents()
+
+
+    def slow_down(self):
+        self.time_delay += 0.01
+        self.time_delay_display -= 0.10
+
+        if self.time_delay >= 0.20:
+            self.time_delay = 0.20
+            self.lbl_speed.setText("-1.00 (Minimum Speed)")
+        elif self.time_delay == 0:
+            self.lbl_speed.setText("0.00 (Default Speed)")
+        else:
+            self.lbl_speed.setText("%.2f" % self.time_delay_display)
+
+
+    def speed_up(self):
+        self.time_delay -= 0.01
+        self.time_delay_display += 0.10
+
+        if self.time_delay <= 0:
+            self.time_delay = 0
+            self.lbl_speed.setText("1.00 (Maximum Speed)")
+        elif self.time_delay == 0:
+            self.lbl_speed.setText("0.00 (Default Speed)")
+        else:
+            self.lbl_speed.setText("%.2f" % self.time_delay_display)
 
 
     def reset_all(self):
@@ -161,9 +216,12 @@ class MainWindow(QMainWindow):
         self.drone_mesh_filename = ""
         self.flight_plan_filename = ""
         self.flight_plan = []
+        self.time_delay = 0.10
+        self.time_delay_display = 0
 
         self.lbl_import_drone.setText("STL file path...")
         self.lbl_import_flight.setText("CSV file path...")
+        self.lbl_speed.setText("0.00 (Default Speed)")
 
 
     def reset_flight(self):
